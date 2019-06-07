878b73942ea87fb7995b7e5f6fed0ebb9abf452c
cjwcommuny <cjwcommuny@outlook.com>
2019-06-07 19:45:51 +0800

    basically finish report

--- src/main/java/manager/FileHandler.java
+++ src/main/java/manager/FileHandler.java
@@ -10,21 +10,11 @@ import java.util.List;
 
 public interface FileHandler {
     List<Info> quit();
-
     void writeTableCatalogToFile(Table table);
-//    void writeIndexToFile(byte[] bytes, String indexName, String tableName);
     void writeTupleToFile(byte[] bytes, String tableName, int offset);
     Collection<Table> readAllTableCatalogsFromFiles();
-//    byte[] readIndex(String indexName);
-//    byte[] readTupleBytes(String tableName);
     ByteCarrier readTupleBytes(String tableName, int offset, int length);
-
-
     void createTable(String tableName);
-
-//    long getTableTuplesTotalLength(String tableName);
-
     void init();
-
     List<Info> deleteTable(String tableName);
 }
